b0f7520be4ad1b0dc726308020c73a04fd2e7e47
Erick <emarticotte@spektrummedia.com>
2019-08-27 14:24:50 -0400

    Remove FirstMigrationProduct

--- src/Domain/Entities/Product.cs
+++ src/Domain/Entities/Product.cs
@@ -4,11 +4,7 @@ namespace Academie.PawnShop.Domain.Entities
 {
     public class Product : EntityBase<Guid>
     {
-        public string Name { get; set; }
 
-        public int Price { get; set; }
-
-        public int Quatity { get; set; }
         public string Name { get; set; }
         public int Quantity { get; set; }
         public double Price { get; set; }
