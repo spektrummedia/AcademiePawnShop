2695d70c60de89056cbaa7c44902a36c59cfc2b0
DESKTOP-DBQLB86\Slemieux <slemieux@spektrummedia.com>
2019-08-27 22:25:58 -0400

    Implement Public shop to display products.

---
+++ src/Web/Pages/Products.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Academie.PawnShop.Domain;
+using Academie.PawnShop.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace Academie.PawnShop.Web.Pages
+{
+    public class ProductsModel : PageModel
+    {
+        private readonly PawnShopDbContext _db;
+
+        public IEnumerable<Product> Products { get; set; }
+
+
+        public ProductsModel(PawnShopDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task OnGetAsync()
+        {
+            Products = await _db.Products.ToListAsync();
+        }
+    }
+}
